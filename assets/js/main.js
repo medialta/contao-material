0894acb693486151918675685be2f86a62c06ecc
Lucas Gehin <lucas@medialta.com>
2015-11-06 16:38:09 +0100

    popup modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,30 @@ $(document).ready(function() {
     $('select').select2();
     $('.tooltipped').tooltip({delay: 50});
 });
+
+var Backend = {
+    openModalIframe: function(e) {
+        $('#modal').html('<iframe src="' + e.url + '" width="100%" height="100%" frameborder="0"></iframe>');
+        $('#modal').openModal();
+        return false;
+        var t = e || {}
+          , a = (window.getSize().y - 180).toInt();
+        (!t.height || t.height > a) && (t.height = a);
+        var n = new SimpleModal({
+            width: t.width,
+            hideFooter: !0,
+            draggable: !1,
+            overlayOpacity: .5,
+            onShow: function() {
+                document.body.setStyle("overflow", "hidden")
+            },
+            onHide: function() {
+                document.body.setStyle("overflow", "auto")
+            }
+        });
+        n.show({
+            title: t.title,
+            contents: '<iframe src="' + t.url + '" width="100%" height="' + t.height + '" frameborder="0"></iframe>'
+        })
+    }
+};

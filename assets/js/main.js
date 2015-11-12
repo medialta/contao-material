9d17fa0146170356d2ee0c420e9dab0225512534
Sébastien Jean <sebastien.jean54@gmail.com>
2015-11-12 14:48:37 +0100

    Rewrite main.js: adding functions to Backend object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,3 @@
-function hideUnnecessaryToggles()
-{
-    var $parent = $('.js-toggle-subpanel').parent()
-
-    $('.js-toggle-subpanel').each(function()
-    {
-        if (!$('#' + $(this).data('toggle')).length)
-        {
-            $(this).detach()
-        }
-    })
-
-    if (!$parent.children().length)
-    {
-        $parent.html('')
-    }
-}
-
-var toggleSubpanel = function(e)
-{
-    e.preventDefault()
-
-    var $toggle = $(e.currentTarget)
-    $toggle.toggleClass('is-active')
-    $subpanel = $('#' + $toggle.data('toggle'))
-
-    $subpanel.toggleClass('is-active').slideToggle()
-
-    if ($subpanel.is('.is-active'))
-    {
-        $('#submit-subpanel').addClass('is-active').slideDown()
-    }
-    else if ($('.js-subpanel.is-active').length === 1)
-    {
-        $('#submit-subpanel').removeClass('is-active').slideUp()
-    }
-}
-
-$(document).ready(function() {
-    $(".button-collapse").sideNav()
-    $('#modules-nav .collapsible-header').click(function(e) { e.preventDefault() })
-
-    hideUnnecessaryToggles()
-    $('.js-toggle-subpanel').click(toggleSubpanel)
-
-    $('select').select2();
-    $('.tooltipped').tooltip({delay: 50});
-});
-
 var Backend = {
     openModalIframe: function(e) {
         $('#modal').html('<iframe src="' + e.url + '" width="100%" height="100%" frameborder="0"></iframe>');
@@ -71,5 +22,62 @@ var Backend = {
             title: t.title,
             contents: '<iframe src="' + t.url + '" width="100%" height="' + t.height + '" frameborder="0"></iframe>'
         })
+    },
+
+    hideUnnecessaryToggles: function()
+    {
+        var $parent = $('.js-toggle-subpanel').parent()
+
+        $('.js-toggle-subpanel').each(function()
+        {
+            if (!$('#' + $(this).data('toggle')).length)
+            {
+                $(this).detach()
+            }
+        })
+
+        if (!$parent.children().length)
+        {
+            $parent.html('')
+        }
+    },
+
+    toggleSubpanel: function(e)
+    {
+        e.preventDefault()
+
+        var $toggle = $(e.currentTarget)
+        $toggle.toggleClass('is-active')
+        $subpanel = $('#' + $toggle.data('toggle'))
+
+        $subpanel.toggleClass('is-active').slideToggle()
+
+        if ($subpanel.is('.is-active'))
+        {
+            $('#submit-subpanel').addClass('is-active').slideDown()
+        }
+        else if ($('.js-subpanel.is-active').length === 1)
+        {
+            $('#submit-subpanel').removeClass('is-active').slideUp()
+        }
+    },
+
+    initialize: function()
+    {
+        Backend.hideUnnecessaryToggles()
+
+        // Bind events
+        $('.js-toggle-subpanel').click(Backend.toggleSubpanel)
     }
-};
+}
+
+$(function() 
+{
+    $(".button-collapse").sideNav()
+    $('#modules-nav .collapsible-header').click(function(e) { e.preventDefault() })
+
+    Backend.initialize()
+
+    $('select').select2()
+    $('.tooltipped').tooltip({delay: 50})
+})

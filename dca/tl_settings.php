eab716aad096d9fa13058525867f59be834fa503
Lucas Gehin <lucas@medialta.com>
2016-01-26 10:08:56 +0100

    add config image cover

---
+++ dca/tl_settings.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   ContaoMaterial
+ * @author    Medialta
+ * @license   GNU/LGPL
+ * @copyright Medialta 2015
+ */
+
+
+\MetaPalettes::appendFields('tl_settings', 'ContaoMaterial', array('cover_image'));
+
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['cover_image'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['cover_image'],
+    'inputType'               => 'fileTree',
+    'eval'                    => array('filesOnly'=>true, 'fieldType'=>'radio'),
+    'sql'                     => "binary(16) NULL"
+);
---
+++ languages/fr/tl_settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['ContaoMaterial_legend'] = 'Contao Material';
+
+$GLOBALS['TL_LANG']['tl_settings']['cover_image'] = array('Image de couverture des pages', 'Sélectionner une image de couverture. Laisser vide pour utiliser l\'image par défaut.');

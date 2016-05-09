5930f7943ef470ac423e188337f1a443582808fc
Lucas Gehin <lucas@medialta.com>
2016-05-09 14:10:46 +0200

    cover image for modules with config

--- config/config.php
+++ config/config.php
@@ -16,3 +16,13 @@
  * Hooks
  */
 $GLOBALS['TL_HOOKS']['postLogin'][] = array('Helper', 'postLogin');
+
+/**
+ * Cover image
+ */
+$GLOBALS['CONTAO_MATERIAL']['COVER_IMAGE']['faq'] = '/system/modules/contao-material/assets/img/cover_faq.jpg';
+$GLOBALS['CONTAO_MATERIAL']['COVER_IMAGE']['settings'] = '/system/modules/contao-material/assets/img/cover_settings.jpg';
+$GLOBALS['CONTAO_MATERIAL']['COVER_IMAGE']['files'] = '/system/modules/contao-material/assets/img/cover_files.jpg';
+$GLOBALS['CONTAO_MATERIAL']['COVER_IMAGE']['calendar'] = '/system/modules/contao-material/assets/img/cover_calendar.jpg';
+$GLOBALS['CONTAO_MATERIAL']['COVER_IMAGE']['news'] = '/system/modules/contao-material/assets/img/cover_news.jpg';
+$GLOBALS['CONTAO_MATERIAL']['COVER_IMAGE']['themes'] = '/system/modules/contao-material/assets/img/cover_themes.jpg';
